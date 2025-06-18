f823261ad62ed1df462e6db598f6bdddce39e38e
Jack-Rais <giorgio.caridi908@gmail.com>
2025-06-18 17:42:39 +0200

    None

--- nvim/init.lua
+++ nvim/init.lua
@@ -6,3 +6,6 @@ vim.o.expandtab = true -- expand tab input with spaces characters
 vim.o.smartindent = true -- syntax aware indentations for newline inserts
 vim.o.tabstop = 4 -- num of space characters per tab
 vim.o.shiftwidth = 4 -- spaces per indentation level
+
+vim.opt.number = true
+vim.opt.relativenumber = true

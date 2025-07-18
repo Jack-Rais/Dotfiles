d316e7b21d7f4539c2d85fb3cb9b8f520c14ac54
Jack-Rais <giorgio.caridi908@gmail.com>
2025-07-18 16:52:17 +0200

    Languages supported modified

--- nvim/lua/configs/lspconfig.lua
+++ nvim/lua/configs/lspconfig.lua
@@ -1,11 +1,10 @@
 require("nvchad.configs.lspconfig").defaults()
 
 local servers = { 
-  "html", 
-  "cssls",
-  "pyright",
-  "rust_analyzer",
+    "html", 
+    "cssls",
+    "pyright",
+    "rust_analyzer",
+    "qmlls"
 }
 vim.lsp.enable(servers)
-
--- read :h vim.lsp.config for changing options of lsp servers 

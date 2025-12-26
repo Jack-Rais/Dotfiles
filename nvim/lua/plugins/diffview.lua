0a20ae4c61c15fb055f4385f25af688867f13bdf
Jack-Rais <giorgio.caridi908@gmail.com>
2025-12-26 18:08:16 +0100

    Added git helpers for nvim

---
+++ nvim/lua/plugins/diffview.lua
@@ -0,0 +1,7 @@
+return {
+    "sindrets/diffview.nvim",
+    keys = {
+        { "<leader>gd", "<cmd>DiffviewOpen<cr>",      desc = "Apri Diffview" },
+        { "<leader>cg", "<cmd>DiffviewClose<cr>",     desc = "Chiudi Diffview" }
+    },
+}

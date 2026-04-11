d6d6f8bc4c239a73b40e7ce814f8b5c50e3c14da
Jack-Rais <giorgio.caridi908@gmail.com>
2026-04-11 16:40:52 +0200

    Added clippy configuration for rust-analyzer

--- nvim/lua/plugins/lsp_config.lua
+++ nvim/lua/plugins/lsp_config.lua
@@ -2,14 +2,6 @@ return {
     "neovim/nvim-lspconfig",
     config = function()
 
-        local servers = {
-            "lua_ls",
-            "pyright",
-            "rust_analyzer"
-        }
-
-        vim.lsp.enable(servers)
-
         vim.api.nvim_create_autocmd("LspAttach", {
             callback = function(args)
                 local client = vim.lsp.get_client_by_id(args.data.client_id)
